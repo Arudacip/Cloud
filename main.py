e4ab4f4452edb0cf21b79f768192c18c136a1811
racass <racass>
2019-04-08 14:49:00 -0300

    Add api method

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, render_template
 from flask_sockets import Sockets
+from jsonConverter import convertToJson
+
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
@@ -9,7 +11,22 @@ sockets = Sockets(app)
 
 @app.route('/')
 def hello():
+    '''
+    Retorna uma página de inicio contendo os membros do grupo
+    '''
     return render_template('index.html')
+
+@app.route('/api/HelloWorld', methods=['POST', 'GET'])
+def api_HelloWorld():
+    '''
+        Requisição simples de hello world para testar a API
+    '''
+    return convertToJson(HelloWorld="Hello World! Retorno em JSON da API de testes!")
+
+
+'''@app.route('/api/<path:path>', methods=['GET']) #Impede que a API seja acessa por browser
+def error_OnlyPost():
+    return render_template('errorOnlyPost.html')'''
 
 
 if __name__ == '__main__':
